f5dcde3431b71f3242979791184b781ad7df3f46
Xiang-Ji615 <xiaoxiang615615@gmail.com>
2023-01-04 13:42:59 +1100

    Time: 124 ms (10.43%), Space: 106 MB (27.83%) - LeetHub

---
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minimumRounds(int[] tasks) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int t : tasks) {
+            map.put(t, map.getOrDefault(t, 0) + 1);
+        }
+        Queue<Map.Entry<Integer, Integer>> maxHeap = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        maxHeap.addAll(map.entrySet());
+        int res = 0;
+        while(!maxHeap.isEmpty()) {
+            Map.Entry<Integer, Integer> e = maxHeap.poll();
+            if(e.getValue() < 2)
+                return -1;
+            res += (e.getValue() + 2)/3;
+        }
+        return res;
+    }
+}
