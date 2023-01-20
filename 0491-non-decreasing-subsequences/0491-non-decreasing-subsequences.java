d3913dc18638d45e5cd131a4c4b31557051818e8
Xiang-Ji615 <xiaoxiang615615@gmail.com>
2023-01-20 14:54:49 +1100

    Time: 13 ms (63.09%), Space: 49.1 MB (69.09%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
@@ -1,21 +1,19 @@
 class Solution {
     public List<List<Integer>> findSubsequences(int[] nums) {
-        Set<List<Integer>> res = new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            dfs(nums, res, new ArrayList<>(), i);
-        }
-        return new ArrayList(res);
+        Set<List<Integer>> set = new HashSet<>();
+        dfs(nums, set, new ArrayList<>(), 0);
+        return new ArrayList<>(set);
     }
     
-    void dfs(int[] nums, Set<List<Integer>> res, List<Integer> temp, int cur){
-        if(temp.size() > 1)
-            res.add(new ArrayList(temp));
-        for(int i=cur;i<nums.length;i++){
-            if(temp.size() == 0 || nums[i] >= temp.get(temp.size()- 1)){
-                temp.add(nums[i]);
-                dfs(nums, res, temp, i+1);
-                temp.remove(temp.size() - 1);
+    void dfs(int[] nums, Set<List<Integer>> res, List<Integer> lst, int cur) {
+        if(lst.size() > 1)
+            res.add(new ArrayList<>(lst));
+        for(int i=cur;i<nums.length;i++) {
+            if(lst.size() == 0 || (lst.size() > 0 && nums[i] >= lst.get(lst.size() - 1))) {
+                lst.add(nums[i]);
+                dfs(nums, res, lst, i + 1);
+                lst.remove(lst.size() - 1);
             }
-        } 
+        }
     }
 }
