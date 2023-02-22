b4374328122ebfd8eebb68d972788aac011b6cf1
Xiang-Ji615 <xiaoxiang615615@gmail.com>
2023-02-22 11:30:14 +1100

    Time: 10 ms (62.60%), Space: 54.2 MB (9.51%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,31 +1,32 @@
 class Solution {
-    public int shipWithinDays(int[] weights, int D) {
+    public int shipWithinDays(int[] weights, int days) {
         int l = 1, r = 0;
-        for(int w : weights){
+        for(int w : weights) {
             r += w;
             l = Math.max(l, w);
         }
-        while(l < r){
+        while(l < r) {
             int m = l + (r - l)/2;
-            if(isValid(weights, m, D)){
+            if(isValid(weights, days, m)) {
                 r = m;
             }else{
-                l = m+1;
+                l = m + 1;
             }
         }
         return l;
     }
     
-    boolean isValid(int[] weights, int m, int d){
-        int tmp = 0, res = 0;
-        for(int w : weights){
-            tmp += w;
-            if(tmp > m){
+    boolean isValid(int[] weights, int d, int m) {
+        int res = 0, cur = 0;
+        for(int w : weights) {
+            cur += w;
+            if(cur > m) {
                 res++;
-                tmp = w;
+                cur = w;
             }
-            if(res > d)
+            if(res > d) {
                 return false;
+            }
         }
         res++;
         return res <= d;
