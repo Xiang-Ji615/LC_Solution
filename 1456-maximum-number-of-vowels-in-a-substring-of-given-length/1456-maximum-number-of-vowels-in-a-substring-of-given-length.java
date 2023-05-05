b6df76b07639fadb276974324f62a5dd5221f201
Xiang-Ji615 <xiaoxiang615615@gmail.com>
2023-05-05 10:53:38 +1000

    Time: 26 ms (41.98%), Space: 43 MB (76.32%) - LeetHub

---
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxVowels(String s, int k) {
+        Set<Character> set = new HashSet<>();
+        set.add('a');
+        set.add('e');
+        set.add('i');
+        set.add('o');
+        set.add('u');
+        int res = 0, l = 0, tmp = 0;
+        for(int r=0;r<s.length();r++) {
+            char c = s.charAt(r);
+            if(set.contains(c))
+                tmp++;
+            while(r - l + 1 > k) {
+                tmp -= set.contains(s.charAt(l++)) ? 1 : 0;
+            }
+            res = Math.max(res, tmp);
+        }
+        return res;
+    }
+}
