f86d63c38bfeb11373c2898e4f461b6f0ca5f5c4
Xiang-Ji615 <xiaoxiang615615@gmail.com>
2023-06-01 12:14:32 +1000

    Time: 19 ms (43.72%), Space: 44.8 MB (22.14%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,31 +1,28 @@
 class Solution {
     
-    int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+    int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {-1, 1}, {1, -1}, {-1, -1}};
     
     public int shortestPathBinaryMatrix(int[][] grid) {
-        if(grid == null || grid.length == 0)
-            return 0;
-        if(grid[0][0] == 1 || grid[grid.length - 1][grid[0].length - 1] == 1)
-            return -1;
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{0, 0});
-        visited[0][0] = true;
+        if(grid[0][0] == 0) q.offer(new int[]{0, 0});
         int res = 1;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        visited[0][0] = true;
         while(!q.isEmpty()) {
             int size = q.size();
-            for(int i=0;i<size;i++){
+            for(int i=0;i<size;i++) {
                 int[] cur = q.poll();
                 if(cur[0] == grid.length - 1 && cur[1] == grid[0].length - 1){
                     return res;
                 }
-                for(int[] d : dirs){
-                    int ni = cur[0] + d[0], nj = cur[1] + d[1];
-                    if(ni >= 0 && ni < grid.length && nj >= 0 && nj < grid[0].length && !visited[ni][nj] && grid[ni][nj] == 0){
+                for(int[] d : dirs) {
+                    int ni = cur[0] + d[0];
+                    int nj = cur[1] + d[1];
+                    if(ni >= 0 && ni < grid.length && nj >= 0 && nj < grid[0].length && !visited[ni][nj] && grid[ni][nj] == 0) {
                         q.offer(new int[]{ni, nj});
                         visited[ni][nj] = true;
                     }
-                }
+                } 
             }
             res++;
         }
