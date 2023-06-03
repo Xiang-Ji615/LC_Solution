d771ad024a2f7cf718fb0c113bebf1ddebef074c
Xiang-Ji615 <xiaoxiang615615@gmail.com>
2023-06-03 20:08:01 +1000

    Time: 121 ms (32.36%), Space: 68.1 MB (7.28%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -1,7 +1,6 @@
 class Solution {
     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
         Map<Integer, List<Integer>> map = new HashMap<>();
-        int total = 0;
         for(int i=0;i<manager.length;i++){
             int j = manager[i];
             map.putIfAbsent(j, new ArrayList<>());
@@ -10,13 +9,14 @@ class Solution {
         return dfs(map, informTime, headID);
     }
     
-    int dfs(Map<Integer, List<Integer>> map, int[] informTime, int cur){
+    int dfs(Map<Integer, List<Integer>> map, int[] arr, int cur) {
         int res = 0;
-        if(!map.containsKey(cur))
+        if(!map.containsKey(cur)) {
             return res;
-        for(int nei : map.get(cur)){
-            res = Math.max(res, dfs(map, informTime, nei));
         }
-        return res + informTime[cur];
+        for(int nei : map.getOrDefault(cur, new ArrayList<>())) {
+            res = Math.max(res, dfs(map, arr, nei));
+        }
+        return res + arr[cur];
     }
 }
