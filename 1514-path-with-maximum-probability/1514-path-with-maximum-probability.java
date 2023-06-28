8aff62825aeb337c15df49a8167c34025e034856
Xiang-Ji615 <xiaoxiang615615@gmail.com>
2023-06-28 12:15:57 +1000

    Time: 96 ms (17.40%), Space: 57 MB (30.41%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
@@ -1,52 +1,39 @@
 class Solution {
     
-    class State{
+    class State {
         int node;
-        double prob;
+        double val;
         
-        public State(int node, double prob){
+        public State(int node, double val) {
             this.node = node;
-            this.prob = prob;
+            this.val = val;
         }
     }
     
     public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
         Map<Integer, Map<Integer, Double>> map = new HashMap<>();
-        for(int i=0;i<edges.length;i++){
+        for(int i=0;i<edges.length;i++) {
             int[] e = edges[i];
             map.putIfAbsent(e[0], new HashMap<>());
             map.putIfAbsent(e[1], new HashMap<>());
             map.get(e[0]).put(e[1], succProb[i]);
             map.get(e[1]).put(e[0], succProb[i]);
         }
-        double[] probs= new double[n];
+        
         Queue<State> q = new LinkedList<>();
         q.offer(new State(start, 1.0));
-        while(!q.isEmpty()){
+        double[] vals = new double[n];
+        while(!q.isEmpty()) {
             State cur = q.poll();
             int parent = cur.node;
-            double prob = cur.prob;
-            for(Map.Entry<Integer, Double> e : map.getOrDefault(parent, new HashMap<>()).entrySet()){
-                if(probs[e.getKey()] >= prob * e.getValue())
+            double v = cur.val;
+            for(Map.Entry<Integer, Double> nei : map.getOrDefault(parent, new HashMap<>()).entrySet()) {
+                if(vals[nei.getKey()] >= v * nei.getValue())
                     continue;
-                q.offer(new State(e.getKey(), prob*e.getValue()));
-                probs[e.getKey()] = prob * e.getValue();
+                q.offer(new State(nei.getKey(), v * nei.getValue()));
+                vals[nei.getKey()] = v * nei.getValue();
             }
         }
-        return probs[end];
+        return vals[end];
     }
-    
-    // void dfs(Map<Integer, Map<Integer, Double>> map, boolean[] visited, int cur, int end, double tmp){
-    //     if(visited[cur])
-    //         return;
-    //     if(cur == end){
-    //         res = Math.max(res, tmp);
-    //         return;
-    //     }
-    //     visited[cur] = true;
-    //     for(Map.Entry<Integer, Double> nei : map.getOrDefault(cur, new HashMap<>()).entrySet()){
-    //         dfs(map, visited, nei.getKey(), end, tmp * nei.getValue());
-    //     }
-    //     visited[cur] = false;
-    // }
 }
