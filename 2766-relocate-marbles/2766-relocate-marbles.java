5cc3d921f91e77757e3b817850c03189aaecddf1
Xiang-Ji615 <xiaoxiang615615@gmail.com>
2023-07-09 08:38:23 +1000

    Time: 88 ms (40.00%), Space: 62 MB (40.00%) - LeetHub

--- 2766-relocate-marbles/2766-relocate-marbles.java
+++ 2766-relocate-marbles/2766-relocate-marbles.java
@@ -1,19 +1,19 @@
 class Solution {
     public List<Integer> relocateMarbles(int[] nums, int[] moveFrom, int[] moveTo) {
-        Map<Integer, Integer> map = new TreeMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         for(int n : nums) 
             map.put(n, map.getOrDefault(n, 0) + 1);
         for(int i=0;i<moveFrom.length;i++) {
             int val = map.getOrDefault(moveFrom[i], 0);
             map.remove(moveFrom[i]);
-            if(map.containsKey(moveTo[i])) {
+            if(map.containsKey(moveTo[i]))
                 map.put(moveTo[i], map.get(moveTo[i]) + val);
-            }else{
+            else
                 map.put(moveTo[i], val);
-            }
         }
         List<Integer> res = new ArrayList<>();
         res.addAll(map.keySet());
+        Collections.sort(res);
         return res;
     }
 }
