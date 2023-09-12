ae9b37d19f9fcaa14c4e147b64e56090eb9b2a6d
Xiang-Ji615 <xiaoxiang615615@gmail.com>
2023-09-12 12:41:30 +1000

    Time: 51 ms (25.39%), Space: 45.1 MB (7.31%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minDeletions(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : s.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        int res = 0;
+        Queue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        maxHeap.addAll(map.values());
+        while(!maxHeap.isEmpty()){
+            int cur = maxHeap.poll();
+            if(maxHeap.isEmpty())
+                return res;
+            if(cur == maxHeap.peek()){
+                if(cur > 1){
+                    maxHeap.offer(--cur);
+                }
+                res++;
+            }
+        }
+        return res;
+    }
+}
