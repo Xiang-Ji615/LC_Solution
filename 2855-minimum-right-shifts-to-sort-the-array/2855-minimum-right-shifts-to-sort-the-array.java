d618e0f7082200e9b86ea951087d175510660e90
Xiang-Ji615 <xiaoxiang615615@gmail.com>
2023-09-17 09:47:57 +1000

    Time: 6 ms (33.33%), Space: 43.5 MB (33.33%) - LeetHub

---
+++ 2855-minimum-right-shifts-to-sort-the-array/2855-minimum-right-shifts-to-sort-the-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minimumRightShifts(List<Integer> nums) {
+        int min = Integer.MAX_VALUE;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.size();i++) {
+            map.put(nums.get(i), i);
+            min = Math.min(min, nums.get(i));
+        }
+        int start = map.get(min);
+        System.out.println(start + "");
+        for(int i=0;i<nums.size() - 1;i++) {
+            if(nums.get(start%nums.size()) > nums.get((start + 1)%nums.size()))
+                return -1;
+            start++;
+        }
+        return map.get(min) == 0 ? 0 : nums.size() - map.get(min);
+    }
+}
