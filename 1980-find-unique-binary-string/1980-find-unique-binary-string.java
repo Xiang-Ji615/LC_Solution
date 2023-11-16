972698e614ef79909b3eede271673054f770a019
Xiang-Ji615 <xiaoxiang615615@gmail.com>
2023-11-16 22:04:52 +1100

    Time: 0 ms (100.00%), Space: 40.1 MB (75.27%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.java
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -1,20 +1,8 @@
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
-        Set<String> set = new HashSet<>();
-        for(String n : nums)
-            set.add(n);
-        for(int i=0;i<=Math.pow(nums.length, 2);i++){
-            String s = convert(i, nums.length);
-            if(!set.contains(s))
-                return s;
-        }
-        return null;
-    }
-    
-    String convert(int n, int l){
         StringBuilder sb = new StringBuilder();
-        for(int i=l-1;i>=0;i--){
-            sb.append((n&1<<i) == 0 ? "1" : "0");
+        for(int i=0;i<nums.length;i++) {
+            sb.append(nums[i].charAt(i) == '0' ? '1' : '0');
         }
         return sb.toString();
     }
