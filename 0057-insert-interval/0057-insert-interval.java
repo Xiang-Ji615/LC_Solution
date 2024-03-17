d7c05c0a6a98372042c5d5988b3ef73c58b67e3e
Xiang-Ji615 <xiaoxiang615615@gmail.com>
2024-03-17 15:14:20 +1100

    Time: 4 ms (11.34%), Space: 45.1 MB (9.20%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -3,26 +3,26 @@ class Solution {
         if(intervals == null || newInterval == null)
             return new int[0][];
         List<int[]> lst = new ArrayList<>();
-        for(int[] i : intervals)
-            lst.add(i);
+        for(int[] it : intervals) {
+            lst.add(it);
+        }
         lst.add(newInterval);
-        Collections.sort(lst, (a, b)->a[0] - b[0]);
+        Collections.sort(lst, (a, b) -> a[0] - b[0]);
         List<int[]> resLst = new ArrayList<>();
         int[] first = lst.get(0);
-        for(int i=1;i<lst.size();i++){
+        for(int i=1;i<lst.size();i++) {
             int[] cur = lst.get(i);
-            if(Math.max(first[0], cur[0]) <= Math.min(first[1], cur[1]))
-                first[1] = Math.max(first[1], cur[1]);
-            else{
+            if(Math.max(first[0], cur[0]) <= Math.min(first[1], cur[1])) {
+                first[1] = Math.max(cur[1], first[1]);
+            }else {
                 resLst.add(first);
                 first = cur;
             }
         }
         resLst.add(first);
         int[][] res = new int[resLst.size()][2];
-        for(int i=0;i<resLst.size();i++){
+        for(int i=0;i<resLst.size();i++) 
             res[i] = resLst.get(i);
-        }
         return res;
     }
 }
