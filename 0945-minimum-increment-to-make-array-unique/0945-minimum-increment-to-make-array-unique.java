334223066f66dcd45f610b47f9f7110bb71d0401
Xiang-Ji615 <xiaoxiang615615@gmail.com>
2024-06-14 18:10:06 +1000

    Time: 38 ms (43.61%), Space: 60.1 MB (44.92%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
@@ -1,10 +1,10 @@
 class Solution {
     public int minIncrementForUnique(int[] A) {
         Arrays.sort(A);
-        int res = 0, need = 0;
-        for(int a : A){
-            res += Math.max(need - a, 0);
-            need = Math.max(a, need) + 1;
+        int res = 0, max = 0;
+        for(int n : A) {
+            res += Math.max(max - n, 0);
+            max = Math.max(n, max) + 1;
         }
         return res;
     }
