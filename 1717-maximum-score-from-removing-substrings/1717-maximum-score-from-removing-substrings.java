122c3575a02631a45d9f9dfaaf2511412d747d5c
Xiang-Ji615 <xiaoxiang615615@gmail.com>
2024-07-12 10:09:11 +1000

    Time: 160 ms (55.56%), Space: 46.7 MB (40.40%) - LeetHub

---
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maximumGain(String s, int x, int y) {
+        Stack<Character> stack = new Stack<>(), stack2 = new Stack<>();
+        int res = 0, max = Math.max(x, y), min = Math.min(x, y);
+        char fir = (x > y) ? 'a' : 'b', sec = (x > y) ? 'b' : 'a';
+        for(char c : s.toCharArray()){
+            if(!stack.isEmpty() && stack.peek() == fir && c == sec){
+                stack.pop();
+                res += max;
+            }else
+                stack.push(c);
+        }
+        while(!stack.isEmpty()) {
+            char c = stack.pop();
+            if(!stack2.isEmpty() && stack2.peek() == fir && c == sec) {
+                stack2.pop();
+                res += min;
+            }else
+                stack2.push(c);
+        }
+        return res;
+    }
+}
