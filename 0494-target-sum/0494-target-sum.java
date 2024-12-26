2096fcecb11cbfcf8a6fd6ee7a12b863c9d8f903
Xiang-Ji615 <xiaoxiang615615@gmail.com>
2024-12-26 12:21:15 +1100

    Time: 10 ms (61.23%), Space: 44.8 MB (27.87%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,19 +1,27 @@
 class Solution {
     
-    int res = 0;
-    
-    public int findTargetSumWays(int[] nums, int target) {
-        dfs(nums, 0, target);
-        return res;
-    }
-    
-    void dfs(int[] nums, int cur, int target) {
-        if(cur == nums.length) {
-            if(target == 0)
-                res++;
-            return;
+    public int findTargetSumWays(int[] nums, int S) {
+        if(nums == null || nums.length == 0)
+            return 0;
+        int sum = 0;
+        for(int n : nums){
+            sum += n;
         }
-        dfs(nums, cur + 1, target - nums[cur]);
-        dfs(nums, cur + 1, target + nums[cur]);
+        if(Math.abs(sum) < Math.abs(S))
+            return 0;
+        int[][] dp = new int[nums.length+1][sum * 2 + 1];
+        dp[0][sum] = 1;
+        for(int i=1;i<dp.length;i++){
+            int n = nums[i-1];
+            for(int j=0;j<dp[0].length;j++){
+                if(j >= n){
+                    dp[i][j] += dp[i-1][j-n];
+                }
+                if(j < dp[0].length - n){
+                    dp[i][j] += dp[i-1][j+n];
+                }
+            }
+        }
+        return dp[dp.length-1][sum + S];
     }
 }
